87a8e900a4e0880964a6b84e985980971ffb00e5
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-04-10 12:28:36 +0200

    Fixing some test

--- src/test/java/features/GeneratingBillsTest.java
+++ src/test/java/features/GeneratingBillsTest.java
@@ -9,18 +9,20 @@ import fr.unice.polytech.isa.dd.BillingGeneratedInterface;
 import fr.unice.polytech.isa.dd.DeliveryInterface;
 import fr.unice.polytech.isa.dd.DeliverySchedule;
 import fr.unice.polytech.isa.dd.NextDeliveryInterface;
-import fr.unice.polytech.isa.dd.entities.Customer;
-import fr.unice.polytech.isa.dd.entities.Delivery;
+import fr.unice.polytech.isa.dd.entities.*;
 import fr.unice.polytech.isa.dd.entities.Package;
-import fr.unice.polytech.isa.dd.entities.Provider;
 import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
 import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -32,11 +34,104 @@ public class GeneratingBillsTest extends AbstractBillingTest {
 
     @PersistenceContext
     private EntityManager entityManager;
+    @Inject
+    private UserTransaction utx;
 
     @EJB(name = "delivery-stateless") NextDeliveryInterface nextDeliveryInterface;
     @EJB(name = "bill-stateless") private BillingGeneratedInterface billinggenerator ;
     @EJB(name = "delivery-stateless") private DeliverySchedule deliverySchedule;
     @EJB(name = "delivery-stateless") private DeliveryInterface deliveryInterface;
+
+    Customer c = new Customer("Pm", "adresse1");
+
+    Provider pro3;
+    Provider pro2;
+
+    Package package5;
+    Package package2;
+    Package package3;
+    Package package4;
+
+    Delivery delivery1;
+    Delivery delivery2;
+    Delivery delivery3;
+    Delivery delivery4;
+
+
+    @Before
+    public void setUp() {
+        entityManager.persist(c);
+
+        pro3 = new Provider();
+
+        pro2 = new Provider();
+
+        package5 = new Package();
+        package5.setWeight(10.0);
+
+        package2 = new Package();
+        package2.setWeight(15.0);
+
+        delivery1 = new Delivery();
+        delivery1.setCustomer(c);
+
+        delivery2 = new Delivery();
+        delivery2.setCustomer(c);
+
+        package3 = new Package();
+        package3.setWeight(20.0);
+
+        package4 = new Package();
+        package4.setWeight(25.0);
+
+        delivery3 = new Delivery();
+        delivery3.setCustomer(c);
+
+        delivery4 = new Delivery();
+        delivery4.setCustomer(c);
+
+
+    }
+
+    @After
+    public void cleanUp() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        utx.begin();
+        delivery1 = entityManager.merge(delivery1);
+        entityManager.remove(delivery1);
+        delivery2 = entityManager.merge(delivery2);
+        entityManager.remove(delivery2);
+        delivery3 = entityManager.merge(delivery3);
+        entityManager.remove(delivery3);
+        delivery4 = entityManager.merge(delivery4);
+        entityManager.remove(delivery4);
+
+        package5 = entityManager.merge(package5);
+        entityManager.remove(package5);
+        package2 = entityManager.merge(package2);
+        entityManager.remove(package2);
+        package3 = entityManager.merge(package3);
+        entityManager.remove(package3);
+        package4 = entityManager.merge(package4);
+        entityManager.remove(package4);
+
+        pro3 = entityManager.merge(pro3);
+        entityManager.remove(pro3);
+        pro2 = entityManager.merge(pro2);
+        entityManager.remove(pro2);
+
+        c = entityManager.merge(c);
+        entityManager.remove(c);
+
+        int size = billinggenerator.get_bills().size();
+        for (int i = 0; i<size; i++){
+           Bill _bill = entityManager.merge(billinggenerator.get_bills().get(0));
+            entityManager.remove(_bill);
+        }
+        /*Bill _bill = entityManager.merge(billinggenerator.get_bills().get(0));
+        entityManager.remove(_bill);*/
+
+        utx.commit();
+    }
 
     @Quand("^l'employé envoie les (\\d+) livraisons du fournisseurs (.*) et (.*)$")
     public void livraisonsenvoyées(Integer arg0,String arg1,String arg2){
@@ -44,67 +139,44 @@ public class GeneratingBillsTest extends AbstractBillingTest {
         for(int i= 0; i < init.getDeliveries().size();i++){
             Delivery delivery = nextDelivery.getNextDelivery();
         }*/
-        Customer c = new Customer("Pm", "adresse1");
-        entityManager.persist(c);
-
-        Provider pro3 = new Provider();
         pro3.setName(arg1);
         entityManager.persist(pro3);
 
-        Provider pro2 = new Provider();
         pro2.setName(arg2);
         entityManager.persist(pro2);
 
-        Package package5 = new Package();
-        package5.setWeight(10.0);
         package5.setProvider(pro3);
         entityManager.persist(package5);
 
-
-        Package package2 = new Package();
-        package2.setWeight(15.0);
         package2.setProvider(pro3);
         entityManager.persist(package2);
         pro3.add(package5);
         pro3.add(package2);
 
-        Delivery delivery1 = new Delivery();
-        delivery1.setCustomer(c);
         delivery1.setPackageDelivered(package5);
         entityManager.persist(delivery1);
 
-        Delivery delivery2 = new Delivery();
-        delivery2.setCustomer(c);
         delivery2.setPackageDelivered(package2);
         entityManager.persist(delivery2);
 
-        Package package3 = new Package();
-        package3.setWeight(20.0);
         package3.setProvider(pro2);
         entityManager.persist(package3);
 
-
-        Package package4 = new Package();
-        package4.setWeight(25.0);
         package4.setProvider(pro2);
         entityManager.persist(package4);
         pro2.add(package3);
         pro2.add(package4);
 
-        Delivery delivery3 = new Delivery();
-        delivery3.setCustomer(c);
         delivery3.setPackageDelivered(package3);
         entityManager.persist(delivery3);
 
-        Delivery delivery4 = new Delivery();
-        delivery4.setCustomer(c);
         delivery4.setPackageDelivered(package4);
         entityManager.persist(delivery4);
-
 
         for(int i= 0; i < 3;i++){
             nextDeliveryInterface.getNextDelivery();
         }
+
 
       /*  for(Provider provider : deliverySchedule.providerList()){
             System.out.println("\n\n/**********************\n"+provider.getName()+" - "+provider.getId()+"\n****************************\n\n");
