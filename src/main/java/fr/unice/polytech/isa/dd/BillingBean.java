c647a5b9197188019f29ce81d86bdd4ed4fe84a3
PaulK <merveille.koffi@etu.unice.fr>
2020-05-13 11:40:09 +0200

    test

--- src/main/java/fr/unice/polytech/isa/dd/BillingBean.java
+++ src/main/java/fr/unice/polytech/isa/dd/BillingBean.java
@@ -26,6 +26,7 @@ import java.util.stream.Collectors;
 public class BillingBean implements BillingGeneratedInterface, CheckTransferStatus {
 
 
+
     @PersistenceContext
     private EntityManager entityManager;
 
